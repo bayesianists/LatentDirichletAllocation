f0169c22ea2067a4047bdace5bcac4c2dc8ff503
Joey Öhman <joeyoh@kth.se>
2020-01-14 19:20:00 +0100

    fixezzz

--- VI.py
+++ VI.py
@@ -7,7 +7,8 @@ import scipy.signal as ss
 """Contains the implemented Variational Inference algorithm for LDA and associated functions"""
 
 NUM_TOPICS_K = 17
-VI_ITERATIONS = 10000
+VI_ITERATIONS = 5
+EM_ITERATIONS = 5
 
 phi = 0
 gamma = 0
@@ -35,23 +36,15 @@ def beta_i_j(phi, documents, i, j):
 
 
 # Called once per doc
-def variational_inference(N, alfa, beta):
+def variational_inference(N, alfa, beta, corpus, V):
     """
     Runs the VI algorithm according to the pseudocode above.
     :param
     :return
     """
 
-    '''
-    phi = init_phi(N)
-    gamma = init_gamma(NUM_TOPICS_K, alfa, N)
-    '''
-
-    vocab = get_vocab()
-    V = len(vocab)
-    corpus = make_corpus(get_vocab())
     NUM_DOCS = len(corpus)
-    phi, gamma, alfa, beta = init_params(corpus, V)
+    phi, gamma = init_phi_gamma(N, alfa)
 
     has_not_converged = True
     t = 0
@@ -106,11 +99,13 @@ def init_beta(V):
         beta.append([1 / V] * V)
     return beta
 
+
 def init_theta(M):
     theta = []
     for i in range(NUM_TOPICS_K):
         theta.append([1 / M] * M)
     return theta
+
 
 def init_alfa_beta(V):
     alfa = [0.5] * NUM_TOPICS_K
@@ -154,10 +149,12 @@ def init_params(corpus, V):
     return phi, gamma, alfa, beta
 '''
 
-#Written in the context of a single document
-#Uses natural logarithm
+
+# Written in the context of a single document
+# Uses natural logarithm
 def gradient_k(theta_average_k, alfa_k, alfa_sum, N):
-    return N*(sp.psi(alfa_sum) - sp.psi(alfa_k) + np.log(theta_average_k))
+    return N * (sp.psi(alfa_sum) - sp.psi(alfa_k) + np.log(theta_average_k))
+
 
 def hessian_inverse_gradient(alfa, document, theta_average_k, M):
     N = len(document)
@@ -168,10 +165,10 @@ def hessian_inverse_gradient(alfa, document, theta_average_k, M):
     gradient = np.array(NUM_TOPICS_K)
     alfa_sum = np.sum(alfa)
     for k in range(NUM_TOPICS_K):
-        Q[k] = -N*sp.polygamma(1, np.sum(alfa))
+        Q[k] = -N * sp.polygamma(1, np.sum(alfa))
         gradient[k] = gradient_k(theta_average_k, alfa[k], alfa_sum, N)
 
-    #id_matrix = np.diag(np.array(NUM_TOPICS_K))
+    # id_matrix = np.diag(np.array(NUM_TOPICS_K))
     inv_Q = np.linalg.inv(Q)
     denominator = 0
     numerator = 0
@@ -182,9 +179,9 @@ def hessian_inverse_gradient(alfa, document, theta_average_k, M):
     """
     denominator += gradient / Q
     numerator += inv_Q
-    b = numerator/(denominator + 1/z)
+    b = numerator / (denominator + 1 / z)
 
-    return (gradient - b)/Q
+    return (gradient - b) / Q
 
 
 def max_step(alfa, beta, phi, V, corpus, theta):
@@ -195,19 +192,22 @@ def max_step(alfa, beta, phi, V, corpus, theta):
 
     theta_averages = row_averages(theta, len(corpus))
     for docIdx in range(len(corpus)):
-        alfa[docIdx] = alfa[docIdx] - hessian_inverse_gradient(alfa[docIdx], corpus[docIdx], theta_averages, len(corpus))
+        alfa[docIdx] = alfa[docIdx] - hessian_inverse_gradient(alfa[docIdx], corpus[docIdx], theta_averages,
+                                                               len(corpus))
 
     return alfa, beta
+
 
 def row_averages(theta, M):
     row_averages = np.array(NUM_TOPICS_K)
     for k in range(NUM_TOPICS_K):
         sum = 0
         for d in range(M):
-            sum += theta[d][k]/M
+            sum += theta[d][k] / M
 
         row_averages[k] = sum
     return row_averages
+
 
 def variational_expectation_maximization():
     vocab = get_vocab()
@@ -216,17 +216,22 @@ def variational_expectation_maximization():
     NUM_DOCS = len(corpus)
     # phi, gamma, alfa, beta = init_params(corpus, V)
     alfa, beta = init_alfa_beta(V)
-    theta = init_theta()
-
-    # phi, gamma, for each doc in list
+    theta = init_theta(NUM_DOCS)
     phi = []
     gamma = []
-    for docIdx in range(NUM_DOCS):
-        doc = corpus[docIdx]
-        N = np.size(doc, axis=0)  # TODO DOUBLE CHECK
-        phi_doc, gamma_doc = variational_inference(N, alfa, beta)
-        phi.append(phi_doc)
-        gamma.append(gamma_doc)
 
-    # Maximization step
-    alfa, beta = max_step(alfa, beta, phi, V, corpus, theta)
+    for emStep in range(EM_ITERATIONS):
+        # phi, gamma, for each doc in list
+        phi = []
+        gamma = []
+        for docIdx in range(NUM_DOCS):
+            doc = corpus[docIdx]
+            N = np.size(doc, axis=0)  # TODO DOUBLE CHECK
+            phi_doc, gamma_doc = variational_inference(N, alfa, beta, corpus, V)
+            phi.append(phi_doc)
+            gamma.append(gamma_doc)
+
+        # Maximization step
+        alfa, beta = max_step(alfa, beta, phi, V, corpus, theta)
+
+    return phi, gamma, alfa, beta, theta
